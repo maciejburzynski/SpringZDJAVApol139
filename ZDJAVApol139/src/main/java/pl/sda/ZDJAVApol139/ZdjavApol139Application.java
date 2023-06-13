8d35fcc7cbb5ad370bd4eade5b0cf619358d25b7
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-06-13 18:56:10 +0200

    Init basic springboot project

---
+++ ZDJAVApol139/src/main/java/pl/sda/ZDJAVApol139/ZdjavApol139Application.java
@@ -0,0 +1,13 @@
+package pl.sda.ZDJAVApol139;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdjavApol139Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdjavApol139Application.class, args);
+	}
+
+}
---
+++ ZDJAVApol139/src/test/java/pl/sda/ZDJAVApol139/ZdjavApol139ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.ZDJAVApol139;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdjavApol139ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
