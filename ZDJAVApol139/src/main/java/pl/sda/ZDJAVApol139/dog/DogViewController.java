375dfe67256b50fe4b449d6f856f873ca5f7dcc9
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-06-27 20:07:57 +0200

    add view of dog list iteration using thymeleaf

--- ZDJAVApol139/src/main/java/pl/sda/ZDJAVApol139/dog/DogViewController.java
+++ ZDJAVApol139/src/main/java/pl/sda/ZDJAVApol139/dog/DogViewController.java
@@ -1,13 +1,19 @@
 package pl.sda.ZDJAVApol139.dog;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class DogViewController {
 
+    private final DogService dogService;
+
     @GetMapping(path = "/dogs")
-    public String getDogs(){
+    public String getDogs(Model model) {
+        model.addAttribute("dogs", dogService.findAllDogs());
         return "dogs";
     }
 }
